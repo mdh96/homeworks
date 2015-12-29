1b848c81e7009ff965b4ed411b3fafd4431d9e52
Geoffrey Mainland <mainland@apeiron.net>
2015-12-29 13:14:36 -0500

    Add homework 2 template.

---
+++ hw2/hw2.rkt
@@ -0,0 +1,51 @@
+;;
+;; DO NOT REMOVE THESE TWO LINES
+;;
+#lang racket
+(provide (all-defined-out))
+
+;;
+;; Problem 1
+;;
+
+(define (iterator rlist)
+  #f)
+
+;;
+;; Problem 2
+;;
+
+(define (stream-seq f rlist)
+  #f)
+
+;;
+;; Problem 3
+;;
+
+(define (scan f z l)
+  #f)
+
+;;
+;; Problem 4
+;;
+
+(define (stream-scan f z l)
+  #f)
+
+;;
+;; Problem 5
+;;
+
+(define (lookup name alist)
+  #f)
+
+;;
+;; Problem 6
+;;
+
+(define (lookup-env name env)
+  #f)
+
+;;
+;; Problem 7: Homework Statistics
+;;
