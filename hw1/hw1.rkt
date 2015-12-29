6d7c449e09ae59a7354e787a3f443b52bb3c99a3
Geoffrey Mainland <mainland@apeiron.net>
2015-12-29 13:14:17 -0500

    Add homework 1 template.

---
+++ hw1/hw1.rkt
@@ -0,0 +1,37 @@
+;;
+;; DO NOT REMOVE THESE TWO LINES
+;;
+#lang racket
+(provide (all-defined-out))
+
+;;
+;; Problem 1
+;;
+
+(define (deep-count l)
+  #f)
+
+;;
+;; Problem 2
+;;
+
+(define (reverse l)
+  #f)
+
+;;
+;; Problem 3
+;;
+
+(define (range rlist)
+  #f)
+
+;;
+;; Problem 4
+;;
+
+(define (seq f rlist)
+  #f)
+
+;;
+;; Problem 5: Homework Statistics
+;;
